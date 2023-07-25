a91340aa4316899ede400bbc6b67aecf0b5485f3
christina <cjkoikara@gmail.com>
2023-07-25 11:31:30 +0530

    Cleanup code
    Cleaned up to improve readability

--- HelloThread.c
+++ HelloThread.c
@@ -3,32 +3,24 @@
 #include<unistd.h> 
 pthread_mutex_t lock;
 
-void * Hello(void* arg)
+void * threadFunc(void* arg)
 {
   pthread_mutex_lock(&lock); 
-    printf("Hello ");
+    printf("%s ", (char*) arg);
     sleep(1);
-    pthread_mutex_unlock(&lock);
-    return 0;
-}
-void * Thread(void* arg)
-{
-    pthread_mutex_lock(&lock);
-    printf("Thread\n");
-    sleep(2);
     pthread_mutex_unlock(&lock);
     return 0;
 }
 int main()
 {
-
+    char* msg1 = "Hello";
+    char* msg2 = "Thread\n";
     pthread_t thread1, thread2;
     // Mutex initialization
     pthread_mutex_init(&lock, NULL);
 
-    pthread_create(&thread1,NULL,Hello,NULL);
-    pthread_create(&thread2,NULL,Thread,NULL);
+    pthread_create(&thread1,NULL,threadFunc,msg1);
+    pthread_create(&thread2,NULL,threadFunc,msg2);
     pthread_join(thread1,NULL);
     pthread_join(thread2,NULL);
 }
-
